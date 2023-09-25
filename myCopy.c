d560722010a23ef26dcb91887aa8dac771190338
Elijah Guzman <portalplaya02@gmail.com>
2023-09-25 18:21:26 -0500

    Added myCopy.c to current workstation

--- myCopy.c
+++ myCopy.c
@@ -1,4 +1,13 @@
+// Elijah Guzman - nze594 - 09/25/2023
+// Description: copies text from 'source1.txt' to 'destination1.txt' and 'source2.txt' to 'destination2.txt'
+// Usage: myCopy source1.txt destination1.txt source2.txt destination2.txt
 #define BUF_SIZE 1024
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
     int Fd1, Fd2, Fd3, Fd4, openFlags;
@@ -16,8 +25,8 @@ int main(int argc, char *argv[]) {
     
     Fd1 = open(argv[1], O_RDONLY);
     Fd2 = open(argv[3], O_RDONLY);
-    Fd3 = open(argv[5], O_RDONLY); /*A*/
-    Fd4 = open(argv[7], O_RDONLY); /*B*/
+    Fd3 = open(argv[5], openFlags, filePerms);
+    Fd4 = open(argv[7], openFlags, filePerms);
 
     while ((num = read(Fd1, buf, BUF_SIZE)/*C*/) > 0) {
         if(write(Fd2, buf, num)/*D*/ != num) {
