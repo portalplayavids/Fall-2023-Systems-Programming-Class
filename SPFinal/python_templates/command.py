11b146d1c801d61ac8f32181259db3b8e45cd3a3
Elijah Guzman <portalplaya02@gmail.com>
2023-12-07 21:15:52 -0600

    Added the Final Project Info.

---
+++ SPFinal/python_templates/command.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+class Command:
+    @staticmethod
+    def run_command(cmd: bytes) -> bytes:
+        # TODO: Implement the method using subprocess.Popen
+        # the popen command might need type str not bytes
+        pass  # this can be removed after you fill out this function
---
+++ SPFinal/python_templates/fileio.py
@@ -0,0 +1,16 @@
+class FileIO:
+    @staticmethod
+    def read_file(file_path="to_read.txt") -> bytes:
+        """
+        This method reads data from a file specified by 'file_path' and returns it.
+        TODO: Implement this method. Use binary mode "rb"
+        """
+        pass
+
+    @staticmethod
+    def write_file(data: bytes, file_path="to_write.txt"):
+        """
+        This method writes 'data' to a file specified by 'file_path'.
+        TODO: Implement this method. Use binary mode "rb"
+        """
+        pass
