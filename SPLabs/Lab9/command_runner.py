0844733ca588e759aea70dda4208fddf5f245ee1
Elijah Guzman <portalplaya02@gmail.com>
2023-12-08 14:09:59 -0600

    completed lab 9

--- SPLabs/Lab9/command_runner.py
+++ SPLabs/Lab9/command_runner.py
@@ -31,10 +31,32 @@ import subprocess
 class CommandRunner:
     def run_command(self, command):
         # "command execution via popen"
-        pass
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+        # Handle errors if needed
+            print(f"Error executing command: {stderr.decode().strip()}")
+        return None
+
     def parse_ls_output(self, output):
         # "parse ls -l output"
-        pass
+        lines = output.strip().split('\n')
+        files = []
+        for line in lines[1:]: # Skip the first line which is the total
+            parts = line.split()
+
+            if len(parts) > 8:
+                files.append(parts[8]) # File name is typically the 9th element
+        return files
+
+
     def parse_ps_output(self, output):
         # "parse ps aux output"
-        pass
+        processes = []
+        lines = output.strip().split('\n')
+        for line in lines[1:]: # Skip the header line
+            parts = line.split()
+            if len(parts) > 10:
+                command = ' '.join(parts[10:]) # Command is the remaining parts after 10th element
+                processes.append(command)
+        return processes
